8cc3e1796b6ebe97acf94e254b62f52510587e3d
Henrique821 <vieiradasilvamouraohenrique@gmail.com>
2025-08-14 10:37:26 -0300

    Add files via upload
    programa pra ver quanto falta para passar de ano

---
+++ notaf.py
@@ -0,0 +1,4 @@
+n1=int(input("coloque sua nota do primeiro bimestre"))
+n2=int(input("coloque sua nota do segundo bimestre"))
+media=((60-(n1*2+n2*2))/2)/3
+print("você precisa tirar nos proximos bimestres",media)
